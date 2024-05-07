e2d9a8151299e49f80c273eb6e90c7bca64538eb
israelLuna13 <c24.e.israel.luna@gmail.com>
2024-05-06 21:57:56 -0600

    se agrego el componente y la logica para hacer la suma de calorias de comida y ejercio , ademas se agrego la diferencia de estas dos

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,17 +1,55 @@
-import React from 'react'
-import { Activity } from '../types'
+import React, { useMemo } from "react";
+import { Activity } from "../types";
+import CalorieDisplay from "./CalorieDisplay";
 
-type CalorieTrackerProps={
- activities: Activity[]
+type CalorieTrackerProps = {
+  activities: Activity[];
+};
+function CalorieTracker({ activities }: CalorieTrackerProps) {
+  //contadores
+  const caloriesConsumed = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
 
-}
-function CalorieTracker({activities}:CalorieTrackerProps ) {
+    [activities]
+  );
+
+  const caloriesBourned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0
+      ),
+
+    [activities]
+  );
+
+  const netCalories = useMemo (() => caloriesConsumed - caloriesBourned , [activities])
+
+
+
+
   return (
-
     <>
-    <h2 className='text-4xl font-black text-white text-center'>Resumen de calorias</h2>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de calorias
+      </h2>
+
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+        <CalorieDisplay calories={caloriesConsumed} text="Consumidas" />
+
+        <CalorieDisplay calories={caloriesBourned} text="Ejercicio" />
+
+        <CalorieDisplay calories={netCalories} text="Diferencia" />
+
+      </div>
     </>
-  )
+  );
 }
 
-export default CalorieTracker
+export default CalorieTracker;
